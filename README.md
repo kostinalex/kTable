47863f385e1d158d05a5c635947969bdfd269c80
Alex <kostin2607@gmail.com>
2023-07-19 17:17:41 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Table Component for Angular apps.
+In: list of objects.
+Out: html table with routing, sorting, pagination, search, download and etc.
---
+++ k-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kTable',
+  templateUrl: './k-table.component.html',
+  styleUrls: ['./k-table.component.scss'],
+})
+export class KTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
